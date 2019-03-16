3d30f1a62b09e61cc72efc785b50ce1abab7622c
Hannes Hochreiner <hannes@hochreiner.net>
2019-03-16 19:19:29 +0100

    replaced axios

--- src/Repo.js
+++ src/Repo.js
@@ -4,25 +4,25 @@ export default class Repo {
   }
 
   async getEquipment() {
-    let q = await axios.get('/api/equipment');
+    let q = await this.req.get('/api/equipment');
 
     return q.data.result.filter(e => e.info !== null);
   }
 
   async getSensors() {
-    let q = await axios.get('/api/sensor');
+    let q = await this.req.get('/api/sensor');
 
     return q.data.result;
   }
 
   async getParameterTypes() {
-    let q = await axios.get('/api/parameter-type');
+    let q = await this.req.get('/api/parameter-type');
 
     return q.data.result;
   }
 
   async getParameters(ts_start, ts_end) {
-    let q = await axios.get(`/api/parameter?ts_start=${ts_start.toISOString()}&ts_end=${ts_end.toISOString()}`);
+    let q = await this.req.get(`/api/parameter?ts_start=${ts_start.toISOString()}&ts_end=${ts_end.toISOString()}`);
 
     return q.data.result;
   }
