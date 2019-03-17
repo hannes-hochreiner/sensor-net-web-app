ba83931d79bc5c4c9877e2e5037c42e85d135caa
Hannes Hochreiner <hannes@hochreiner.net>
2019-03-17 20:17:48 +0100

    added tabs and increased amount of data pulled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 // import './App.css';
 import { default as axios } from 'axios';
 import Repo from './Repo.mock';
+import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
+import classnames from 'classnames';
 import EquipmentList from './EquipmentList';
 
 class App extends Component {
@@ -10,9 +12,17 @@ class App extends Component {
     super(props);
     this.state = {
       equipment: [],
-      parameter_types: []
+      parameter_types: [],
+      activeTab: '1'
     };
+    this.switchTab = this.switchTab.bind(this);
     this.repo = new Repo(axios);
+  }
+
+  switchTab(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({activeTab: tab});
+    }
   }
 
   componentDidMount() {
@@ -20,7 +30,7 @@ class App extends Component {
       let equipment = await this.repo.getEquipment();
       let parameter_types = await this.repo.getParameterTypes();
       let ts_start = new Date();
-      ts_start.setMinutes(ts_start.getMinutes() - 2);
+      ts_start.setMinutes(ts_start.getMinutes() - 20);
       let parameter = await this.repo.getParameters(ts_start, new Date());
       let param_equip_dict = parameter.reduce((acc, curr) => {
         if (!acc.hasOwnProperty(curr._equipment_id)) {
@@ -71,9 +81,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <EquipmentList equipment={this.state.equipment} parameter_types={this.state.parameter_types}></EquipmentList>
-        </header>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.switchTab('1'); }}
+            >
+              Values
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => { this.switchTab('2'); }}
+            >
+              Graphs
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <EquipmentList equipment={this.state.equipment} parameter_types={this.state.parameter_types}></EquipmentList>
+          </TabPane>
+          <TabPane tabId="2">
+            empty
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
