ffbd44016e8bab73dbcdc9c15336e67fa1060e82
Hannes Hochreiner <hannes@hochreiner.net>
2019-06-30 21:09:41 +0200

    first working version

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,49 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Sensor-Net</span>
+        <span class="font-weight-light">Web-App</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld :init="init"/>
+      <HelloWorld :init="init" :equipment="equipment" :parametertypes="parametertypes" :parameters="parameters"/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld';
+import Repo from './Repo';
+import {default as axios} from 'axios';
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   },
-  created: function () {
-    setTimeout(function () {
-      console.log(this);
-      this.init = false;
-    }.bind(this), 3000);
+  created: async function () {
+    this.repo = new Repo(axios);
+    this.equipment = await this.repo.getEquipment();
+    this.parametertypes = await this.repo.getParameterTypes();
+    let endTs = new Date();
+    let startTs = new Date();
+    startTs.setMinutes(endTs.getMinutes() - 30);
+    this.parameters = await this.repo.getParameters(startTs, endTs);
+    this.init = false;
+    setInterval(async function() {
+      let endTs = new Date();
+      let startTs = new Date();
+      startTs.setMinutes(endTs.getMinutes() - 30);
+      this.parameters = await this.repo.getParameters(startTs, endTs);
+    }.bind(this), 60000);
   },
   data () {
     return {
-      init: true
+      init: true,
+      equipment: [],
+      parametertypes: [],
+      parameters: []
     }
   }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,148 +4,70 @@
       text-xs-center
       wrap
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
       <v-flex mb-5 xs12>
         <v-progress-circular v-if="init"
           :indeterminate="true">
         </v-progress-circular>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+      <v-expansion-panel>
+        <v-expansion-panel-content
+          v-for="equ in equipment"
+          :key="equ._id"
+        >
+          <template v-slot:header>
+            <container>
+              <v-layout text-xs-center row>
+                <v-flex xs12>
+                  <h3>{{equ.info}}</h3>
+                </v-flex>
+              </v-layout>
+              <v-layout row wrap>
+                <v-layout xs12 md6 v-for="pt in parametertypes" :key="pt._id">
+                  <v-flex xs4 md2>
+                    <v-avatar v-if="pt.id == 'temperature'" :tile="true" :size="35">
+                      <img src="icon_temp.svg" alt="avatar">
+                    </v-avatar>
+                    <v-avatar v-else-if="pt.id == 'pressure'" :tile="true" :size="35">
+                      <img src="icon_press.svg" alt="avatar">
+                    </v-avatar>
+                    <v-avatar v-else-if="pt.id == 'humidity'" :tile="true" :size="35">
+                      <img src="icon_hum.svg" alt="avatar">
+                    </v-avatar>
+                    <div v-else xs4 md2>{{pt.id}}</div>
+                  </v-flex>
+                  <v-flex xs5 md3>{{formatNumber(parameters.filter(e => e._equipment_id == equ._id && e._parameter_type_id == pt._id).sort((a, b) => { return a.ts.localeCompare(b.ts); })[0].val)}}</v-flex>
+                  <v-flex xs3 md1>{{pt.unit}}</v-flex>
+                </v-layout>
+              </v-layout>
+            </container>
+          </template>
+          <v-card>
+            <v-card-text>===</v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
   export default {
-    props: ['init'],
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+    props: ['init', 'equipment', 'parametertypes', 'parameters'],
+    data: () => ({}),
+    methods: {
+      'formatNumber': function (num) {
+        let res = Math.round(num * 10) / 10;
+        let tok = res.toString().split('.');
+
+        while (tok[1].length < 2) {
+          tok[1] += '0';
         }
 
-      ]
-    })
+        return tok.join('.');
+      }
+    }
   }
 </script>
 
