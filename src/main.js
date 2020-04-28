6d68147ed8f6bfeecdf98673bb96a8cd6550d3fb
Hannes Hochreiner <hannes@hochreiner.net>
2020-04-28 21:22:02 +0200

    changed from json to js file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios';
 
 // Import the Auth0 configuration
 // import { domain, clientId } from "../auth_config.json";
@@ -9,30 +8,23 @@ import axios from 'axios';
 // Import the plugin here
 import { Auth0Plugin } from "./auth";
 
-console.log(process.env);
+const {domain, clientId} = window.auth_config;
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
-async function init() {
-  const {domain, clientId} = (await axios.get('/auth_config.json')).data;
-  
-  // Install the authentication plugin here
-  Vue.use(Auth0Plugin, {
-    domain,
-    clientId,
-    onRedirectCallback: appState => {
-      router.push(
-        appState && appState.targetUrl
-          ? appState.targetUrl
-          : window.location.pathname
-      );
-    }
-  });
-  
-  Vue.config.productionTip = false
-  
-  new Vue({
-    router,
-    render: h => h(App)
-  }).$mount('#app')
-}
+Vue.config.productionTip = false
 
-init();
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
